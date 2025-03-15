16ad3320f452b64b322b656c1f8df8a0cb1eebef
Markov Alexey Leonidovich <151729738+aleksejMarkovGit@users.noreply.github.com>
2025-03-15 12:17:34 +0300

    Update IArray.h

--- IArray.h
+++ IArray.h
@@ -8,7 +8,6 @@ namespace linAlg {
 	template<typename T>
 	struct AllowedType : std::false_type {};
 
-	//Перечисляем типы с которыми может работать класc
 	template<>
 	struct AllowedType<signed char> : std::true_type {};
 
@@ -32,15 +31,15 @@ namespace linAlg {
 		static_assert(AllowedType<T>::value, "Invalid type, use int, float and double");
 
 	public:
-		//получить количесвто строк
+
 		virtual size_t getRows() const = 0;
-		//получить количество столбцов
+
 		virtual size_t getColumns() const = 0;
-		//Получит значение по индексу
+
 		virtual T getValue(const size_t rowIndex, const size_t columnIndex) const = 0;
-		//Установить значение
+
 		virtual void setValue(const T value, const size_t rowIndex, const size_t columnIndex) = 0;
-		//Получить данные из массива
+
 		virtual const std::vector<T>& getData() const = 0;	
 	};
 
